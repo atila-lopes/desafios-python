709e25d5aace836966b92c473153f73e99b31100
atila-lopes <atilalopes500@gmail.com>
2026-03-31 21:08:05 -0300

    feat: Resolução do desafio 097

---
+++ d097/main.py
@@ -0,0 +1,9 @@
+def escreva(text):
+    print('~' * (len(text) + 2))
+    print(text.center(len(text) + 2))
+    print('~' * (len(text) + 2))
+
+
+escreva('Gustavo Guanabara')
+escreva('Curso de Python no YouTube')
+escreva('CeV')
